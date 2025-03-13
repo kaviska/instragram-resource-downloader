3dc069f52b234f6d3a20173c210a77a150772980
kaviska <126580314+kaviska@users.noreply.github.com>
2025-03-13 15:06:45 +0530

    updated

--- frontend/src/app/lib/sanity.ts
+++ frontend/src/app/lib/sanity.ts
@@ -9,4 +9,4 @@ export const client = createClient({
 });
 
 const builder = imageUrlBuilder(client);
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source: String) => builder.image(source);
