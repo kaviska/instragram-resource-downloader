3f228b0c86d13f3fadef3e10f2543762007a67d9
kaviska <126580314+kaviska@users.noreply.github.com>
2025-03-23 00:12:29 +0530

    updated
    done

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,29 +1,20 @@
 import Logo from '../../public/logo.png';
 import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Nav() {
   return (
-  
-      
-
-<nav className=" ">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <div>
-      <a href="#" className="text-lg font-bold flex gap-3">
-        <Image src={Logo} alt="logo" width={60} height={60}></Image>
-        <h1 className='text-[#DA08C9] md:text-[24px] text-[20px] mt-4 '>SaveFromInsta</h1>
-
-      </a>
-    </div>
-    <div></div>
-    <div>
-      
-    </div>
-
-  
-  </div>
-</nav>
-
-  
+    <nav className=" ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div>
+          <Link href="/" className="text-lg font-bold flex gap-3">
+            <Image src={Logo} alt="logo" width={60} height={60} />
+            <h1 className='text-[#DA08C9] md:text-[24px] text-[20px] mt-4 '>SaveFromInsta</h1>
+          </Link>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
   );
-  
 }
